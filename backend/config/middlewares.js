c0b7751b060bf88f4b5aa58e6bf1f0e1c0cc3352
kulla-kukkai <kuk.kullavanee@gmail.com>
2026-09-04 02:12:18 +0200

    fix config.js

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -2,7 +2,14 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://book-ducks.netlify.app'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
